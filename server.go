f5089f7405de96af6fbb4fdcfd5387f495ba9aa1
Jonathan Dursi <jonathan@dursi.ca>
2017-12-18 16:48:23 -0500

    Add HTTP API via gRPC reverse gateway

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "flag"
+  "net/http"
+
+  "github.com/golang/glog"
+  "golang.org/x/net/context"
+  "github.com/grpc-ecosystem/grpc-gateway/runtime"
+  "google.golang.org/grpc"
+
+  gw "github.com/ljdursi/candig_mds_pb_py/candig"
+)
+
+var (
+  metadataEndpoint = flag.String("metadata_endpoint", "localhost:50051", "endpoint of metadta service")
+)
+
+func run() error {
+  ctx := context.Background()
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+
+  mux := runtime.NewServeMux()
+  opts := []grpc.DialOption{grpc.WithInsecure()}
+  err := gw.RegisterMetadataServiceHandlerFromEndpoint(ctx, mux, *metadataEndpoint, opts)
+  if err != nil {
+    return err
+  }
+
+  return http.ListenAndServe(":8080", mux)
+}
+
+func main() {
+  flag.Parse()
+  defer glog.Flush()
+
+  if err := run(); err != nil {
+    glog.Fatal(err)
+  }
+}
